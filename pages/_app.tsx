5a60975c240f8bc6eac81383dedb039e9318ca5d
Antoniely Robles <antonielyrobles@gmail.com>
2023-03-29 00:49:07 -0400

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { RegisterModal } from "@/components/modals/RegisterModal";
+import { LoginModal } from "@/components/modals/LoginModal";
+import {EditModal} from "@/components/modals/EditModal";
+import Layout from "@/components/Layout";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
+      <RegisterModal />
+      <LoginModal />
+      <EditModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
